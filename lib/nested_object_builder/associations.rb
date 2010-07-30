2fa0470890d7b4e77f2eb7fb10875fd38133c2b6
Kosuke Matsuda <kosukematsuda@gmail.com>
2010-07-30 10:25:00 +0900

    他のassociationで利用するかもしれないので定数名の変更

--- lib/nested_object_builder/associations.rb
+++ lib/nested_object_builder/associations.rb
@@ -1,15 +1,13 @@
 module ActiveRecord
   module Associations # :nodoc:
-    HAS_MANY_BUILDER_ASSOCIATION_KEYS = [:builder_count, :builder, :builder_include, :builder_order]
+    BUILDER_ASSOCIATION_KEYS = [:builder_count, :builder, :builder_include, :builder_order]
 
     def self.included(base)
       base.extend(ClassMethods)
     end
 
     module ClassMethods
-      @@valid_keys_for_has_many_association << HAS_MANY_BUILDER_ASSOCIATION_KEYS
+      @@valid_keys_for_has_many_association << BUILDER_ASSOCIATION_KEYS
     end
   end
 end
-
-# ActiveRecord::Base.send :include, ActiveRecord::Associations
